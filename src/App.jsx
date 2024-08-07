3cc2a8cd52e243339b019214de742a94789639e4
Searcher Ryan <ryan.2007palmeiras@gmail.com>
2024-08-07 11:30:41 -0300

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import {useState} from 'react'
 import './App.css'
 import { Router, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
 
 const App = () => {
 
+const [color] = useState("#0000FF")
+document.body.style.backgrounColor = color
+
+
   return (
     <Routes>
-      <Route pat="/" />
+      <Route path="/"  element={<Listagem/>}/>
     </Routes>
   )
 }
---
+++ src/pages/Pizzas.jsx
@@ -0,0 +1,42 @@
+import Calabresa from "../assets/images/Calabresa.png"
+import Frango from "../assets/images/Frango.png"
+import Margherita from "../assets/images/Margherita.png"
+import Peperoni from "../assets/images/Peperoni.png"
+import Queijo from "../assets/images/Queijo.png"
+
+
+const Pizzas = [
+    {
+        "id": 1,
+        "nome": "Margherita",
+        "descrição": "Uma pizza clássica com molho de tomate, queijo mozzarella e manjericão fresco.",
+        "imagem": Margherita
+    },
+    {
+        "id": 2,
+        "nome": "Peperoni",
+        "descrição": "Coberta com molho de tomate, queijo mozzarella e fatias de pepperoni crocante.",
+        "imagem": Peperoni
+    },
+    {
+        "id": 3,
+        "nome": "Quatro Queijos",
+        "descrição": "Uma combinação irresistível de queijo mozzarella, cheddar, parmesão e gorgonzola.",
+        "imagem": Queijo
+    },
+    {
+        "id": 4,
+        "nome": "Calabresa",
+        "descrição": "Pizza com molho de tomate, queijo mozzarella e rodelas de calabresa.",
+        "imagem": Calabresa
+    },
+    {
+        "id": 5,
+        "nome": "Frango com Catupiry",
+        "descrição": "Pizza com molho de tomate, queijo mozzarella, frango desfiado e catupiry.",
+        "imagem": Frango
+    },
+  
+]
+
+export default Pizzas;
